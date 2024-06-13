a9f8684ffd9c76f911dac562f7192e68599be6ac
moualhi zine el abidine <zmoualhi@outlook.com>
2024-06-13 19:14:36 +0100

    feat: added logging
    Signed-off-by: moualhi zine el abidine <zmoualhi@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,55 @@ use std::convert::Infallible;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 use std::path::PathBuf;
+use tracing::{error, info, trace, warn};
 
 async fn serve_file(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let path = req.uri().path().trim_start_matches('/');
-
     let mut file_path = PathBuf::from("cdn_root"); // Root directory for the CDN files
     file_path.push(path);
 
+    trace!("Received request for file: {}", file_path.display());
+
     match File::open(&file_path).await {
         Ok(mut file) => {
+            info!("File opened successfully: {}", file_path.display());
             let mut contents = Vec::new();
             match file.read_to_end(&mut contents).await {
-                Ok(_) => Ok(Response::new(Body::from(contents))),
-                Err(_) => Ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(Body::from("Internal Server Error"))
-                    .unwrap()),
+                Ok(_) => {
+                    trace!("File read successfully: {}", file_path.display());
+                    Ok(Response::new(Body::from(contents)))
+                }
+                Err(e) => {
+                    error!("Error reading file: {}", e);
+                    Ok(Response::builder()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .body(Body::from("Internal Server Error"))
+                        .unwrap())
+                }
             }
         }
-        Err(_) => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("File Not Found"))
-            .unwrap()),
+        Err(e) => {
+            warn!("File not found: {}", e);
+            Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("File Not Found"))
+                .unwrap())
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Initialize the tracing subscriber
+    tracing_subscriber::fmt::init();
+
     let make_svc = make_service_fn(|_conn| {
         async { Ok::<_, Infallible>(service_fn(serve_file)) }
     });
-
     let addr = ([0, 0, 0, 0], 3000).into();
-
     let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
+    info!("Listening on http://{}", addr);
     if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
+        error!("Server error: {}", e);
     }
 }
